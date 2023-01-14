22f5422c7d3f39cc5ae2c5bbf06361b0c9349ef4
XPDKasun <kasunx.kelaniya@gmail.com>
2023-01-15 01:54:16 +0530

    Sync

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 const config = {
     typescript: {
         tsConfigPath: 'tsconfig.json'
+    },
+    images: {
+        unoptimized: true
     }
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 function Index() {
     return (
         <div className="container">
-            
+            <Image src="/images/background.jpg" alt="test" fill={true} style={{zIndex: -1}}/>
         </div>
     )
 }
